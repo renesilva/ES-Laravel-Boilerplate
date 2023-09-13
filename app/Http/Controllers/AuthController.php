3c66ac10215ffa2d51e58031e97cffbbf037a26e
ERESSEA RS <rsilva@eresseasolutions.com>
2023-09-13 15:31:43 -0400

    Auth Refresh puede ser accedido sin token válido para refrescar el token.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
   public function __construct()
   {
-    $this->middleware('auth:api', ['except' => ['login']]);
+    $this->middleware('auth:api', ['except' => ['login', 'refresh']]);
   }
 
   public function login(Request $request): JsonResponse
