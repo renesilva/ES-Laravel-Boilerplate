7afdef660a4ad5803a11fe285293284e58ccf49b
Rene Silva <rsilva@eresseasolutions.com>
2023-09-22 11:18:02 -0400

    Testing con base de datos

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+  use CreatesApplication;
+
+  protected function initDatabase(): void
+  {
+
+    Artisan::call('migrate --seed');
+    Artisan::call('app:user-examples-init');
+  }
+
+  protected function resetDatabase(): void
+  {
+    Artisan::call('migrate:reset');
+  }
 }
